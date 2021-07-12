d7ba2400d7ba161a9a4c043df663fa64a7854b20
Werner van der Merwe <wvdm1217@gmail.com>
2021-07-12 19:30:36 +0200

    added wallet panel selection

--- frontend/src/components/MyWallet.vue
+++ frontend/src/components/MyWallet.vue
@@ -7,8 +7,50 @@
         </h1>
 
         <p class="subheading font-weight-regular">
-          My Wallet
+          My Wallets
         </p>
+
+        
+
+        <v-expansion-panels focusable>
+          <v-expansion-panel
+          >
+            <v-expansion-panel-header>Bitcoin</v-expansion-panel-header>
+            <v-expansion-panel-content class="mt-2">
+              
+              
+              Wallet 1
+            </v-expansion-panel-content>
+            <v-divider></v-divider>
+            <v-expansion-panel-content class="mt-2">
+              
+              Wallet 2
+            </v-expansion-panel-content>
+            <v-divider></v-divider>
+            <v-expansion-panel-content class="mt-2">
+              Wallet 3
+            </v-expansion-panel-content>
+            
+          </v-expansion-panel>
+          <v-expansion-panel
+          >
+            <v-expansion-panel-header>Ethereum</v-expansion-panel-header>
+            <v-expansion-panel-content class="mt-2">
+              
+              
+              Wallet 1
+            </v-expansion-panel-content>
+            <v-divider></v-divider>
+            <v-expansion-panel-content class="mt-2">
+              
+              Wallet 2
+            </v-expansion-panel-content>
+            <v-divider></v-divider>
+            <v-expansion-panel-content class="mt-2">
+              Wallet 3
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-col>
 
 
