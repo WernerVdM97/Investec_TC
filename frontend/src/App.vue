1bf8bc59bc99d52b6d8eec5cc4aa7f25c6310170
Werner van der Merwe <wvdm1217@gmail.com>
2021-07-12 17:04:53 +0200

    Working on the mockup in vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,50 @@
 <template>
   <v-app>
+    <v-app-bar
+      app
+      color="blue-grey darken-4"
+      dark
+    >
+      
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        :src="require('./assets/investec-logo-vector.svg')"
+        width="200"
+      />
+
+        
+      
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Logout</span>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Menu</span>
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>My Wallets</v-tab>
+          <v-tab>Send</v-tab>
+          <v-tab>History</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
+
     <v-main>
       <HelloWorld/>
     </v-main>
@@ -7,7 +52,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
@@ -19,5 +64,5 @@ export default {
   data: () => ({
     //
   }),
-}
+};
 </script>
